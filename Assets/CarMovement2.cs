9e25c08a61f10633cd85b9bde8abd136f72b4b08
BrandonStock1 <46919113@est.ort.edu.ar>
2022-06-02 09:02:21 -0300

    avance tp cuatimestral

---
+++ Assets/CarMovement2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement2 : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementSpeed;
+        movementSpeed = 1;
+        float rotationSpeed;
+        rotationSpeed = 1;
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+    }
+}
---
+++ Assets/Follow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Follow : MonoBehaviour
+{
+    public Text txt_time;
+    public Text txt_timeFloored;
+    public Text txt_min;
+    public Text txt_seg;
+
+
+    public float elapsedTime;
+    public bool isCounting;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+        isCounting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float CurrentTime = Time.time;
+
+        txt_time.text = CurrentTime.ToString();
+        
+        txt_timeFloored = Mathf.Floor(CurrentTime).ToString();
+        
+        //contador 
+        txt_min.text = Mathf.Floor(CurrentTime /60).ToString();
+
+        if (isCounting)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+    }
+}
