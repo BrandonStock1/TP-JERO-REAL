02a413fe4cf19ff0477aeb141d6b1c832c382ed3
BrandonStock1 <46919113@est.ort.edu.ar>
2022-06-13 10:11:25 -0300

    correcciones

--- Assets/Scipts/Bombs.cs
+++ Assets/Scipts/Bombs.cs
@@ -9,14 +9,15 @@ public class Bombs : MonoBehaviour
 
     public GameObject Clone;
     public int Cantidad;
-    public Text txtCantClon;
+    //public Text txtCantClon;
 
     public void CloneObject()
     {
-        Cantidad = int.Parse(txtCantClon.text);
+        //Cantidad = int.Parse(txtCantClon.text);
         int contador = 0;
         while (contador < Cantidad)
         {
+            transform.position = new Vector3(-0.12f, 0.25f, 0.12f);
             Instantiate(Clone);
             contador++;
         }
