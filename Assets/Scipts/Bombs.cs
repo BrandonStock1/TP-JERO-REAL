e91ff6b679a8fd513adfca52735dd8f060700c61
BrandonStock1 <46919113@est.ort.edu.ar>
2022-06-28 10:14:40 -0300

    un 8

--- Assets/Scipts/Bombs.cs
+++ Assets/Scipts/Bombs.cs
@@ -7,8 +7,8 @@ using UnityEngine.UI;
 public class Bombs : MonoBehaviour
 {
 
-    public GameObject Clone;
-    public int Cantidad;
+    int CantCubos = 0;
+    public GameObject Cube;
 
     public GameObject txtGanaste;
     public GameObject txtGanaste2;
@@ -21,25 +21,19 @@ public class Bombs : MonoBehaviour
     
     void Update()
     {
-        if (txtGanaste == false)
-        {
-            CloneObject();
-        }
-        if (txtGanaste2 == false)
-        {
-            CloneObject();
-        }
+       
     }
 
-    public void CloneObject()
+    public void OnColissionEnter(Colission Col)
     {
-        int contador = 0;
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Col.gameObject.tag == "Auto" || Col.gameObject.tag == "Auto2")
         {
-            while (contador < Cantidad)
+            txtGanaste.SetActive(true);
+            gameObject.SetActive(false);
+            while (CantCubos < 100)
             {
-                Instantiate(Clone);
-                contador++;
+                Instantiate(Cube);
+                CantCubos++;
             }
         }
     }
